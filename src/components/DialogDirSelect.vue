cae41bf101e5fd44d9bb598d003dca0ee32ba2d4
hayato-SMZ <aegis.tkx@gmail.com>
2023-12-12 07:49:10 +0900

    URLを指定するためのダイアログの追加

---
+++ src/components/DialogDirSelect.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const path = ref("");
+</script>
+
+<template>
+  <v-dialog>
+    <v-card>
+      <v-text-field></v-text-field>
+    </v-card>
+  </v-dialog>
+</template>
